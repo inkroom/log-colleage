21f522fa4a62cd66d33ae80e65aefb1aa391a7d6
inkroom <245900986@qq.com>
2019-06-04 00:24:11 +0800

    修改引用

--- web/src/main/java/cn/inkroom/log/web/service/StatisticsService.java
+++ web/src/main/java/cn/inkroom/log/web/service/StatisticsService.java
@@ -2,7 +2,6 @@ package cn.inkroom.log.web.service;
 
 import cn.inkroom.log.web.bean.Statistics;
 import cn.inkroom.log.web.dao.StatisticsDao;
-import com.sun.xml.internal.bind.v2.TODO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
